2fdf837f3d23a757b03cb2d793a30f6f73c35c08
facundo2309 <facundoomargarcia@gmail.com>
2022-12-05 20:12:22 -0300

    Convection selector

---
+++ Front/ChangeCoin/src/Logica/showMultipleSelector.java
@@ -0,0 +1,25 @@
+package Logica;
+import javax.swing.JOptionPane;
+
+public class showMultipleSelector {
+
+private final OptionsConvertion optionOne = OptionsConvertion.Change_Digital_Currency;
+private final OptionsConvertion optionTwo = OptionsConvertion.Exchange_money;
+
+    public String showMultipleSelctorValues(){
+        Object[] possibleValues = {
+                optionOne.toString().replace("_"," "),
+                optionTwo.toString().replace("_"," ")
+        };
+        Object selectedValue = JOptionPane.showInputDialog(null,
+                "Choose a conversion option",
+                "Menu",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                possibleValues, "Select one choose"
+        );
+        return selectedValue.toString();
+    }
+
+
+}
