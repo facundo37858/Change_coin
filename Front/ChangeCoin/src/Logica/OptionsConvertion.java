b67e5f78bf3f7e8fb0dab9b37aee90e6ba061152
facundo2309 <facundoomargarcia@gmail.com>
2022-12-05 20:11:40 -0300

    Convection selector

---
+++ Front/ChangeCoin/src/Logica/OptionsConvertion.java
@@ -0,0 +1,5 @@
+package Logica;
+
+public enum OptionsConvertion {
+    Exchange_money, Change_Digital_Currency
+}
--- Front/ChangeCoin/src/Main.java
+++ Front/ChangeCoin/src/Main.java
@@ -1,9 +1,13 @@
 import Logica.showInputDialog;
+import Logica.showMultipleSelector;
 
 public class Main {
     public static void main(String[] args) {
-       showInputDialog input = new showInputDialog("Enter amount of money to convert");
-       String value = input.getValue();
+        showMultipleSelector options = new showMultipleSelector();
+        String option = options.showMultipleSelctorValues();
+        System.out.println(option);
+        showInputDialog input = new showInputDialog("Enter amount of money to convert");
+        String value = input.getValue();
         System.out.println(value);
 
 
