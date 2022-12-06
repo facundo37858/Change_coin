7f6a3e4c3c06014bb86addf93756f94977de08c1
facundo2309 <facundoomargarcia@gmail.com>
2022-12-06 15:23:27 -0300

    Workflow

--- Front/ChangeCoin/src/Main.java
+++ Front/ChangeCoin/src/Main.java
@@ -1,18 +1,26 @@
+import Logica.Currencies;
+import Logica.OptionsConvertion;
 import Logica.ShowInputDialog;
 import Logica.ShowMultipleSelector;
 
 public class Main {
     public static void main(String[] args) {
-        ShowMultipleSelector options = new ShowMultipleSelector();
-        String option = options.showMultipleSelctorValues();
-        System.out.println(option);
-        ShowInputDialog input = new ShowInputDialog("Enter amount of money to convert");
-        String value = input.getValue();
+
+        OptionsConvertion[] optionConvertions = OptionsConvertion.values();
+        ShowMultipleSelector optionsConvertions = new ShowMultipleSelector(optionConvertions);
+        String optionConvertSelected = optionsConvertions.showMultipleSelctorValues("Choose a conversion option");
+
+        ShowInputDialog input = new ShowInputDialog("Enter value");
+        int value = Integer.parseInt(input.getValue());
+
+        Currencies[] optionCurrency = Currencies.values();
+        ShowMultipleSelector optionsCurrency = new ShowMultipleSelector(optionCurrency);
+        String optionCurrencySelected = optionsCurrency.showMultipleSelctorValues("Convert to:");
+
+       double converted = optionsCurrency.convetion(optionCurrencySelected);
+        System.out.println(converted);
         System.out.println(value);
-
-
-
-
+        System.out.println(value*converted);
 
     }
 }
