e527620c8e91bd5ba59ad0651461256a3a968076
facundo2309 <facundoomargarcia@gmail.com>
2022-12-06 15:17:28 -0300

    comment

--- Front/ChangeCoin/src/Logica/OptionsConvertion.java
+++ Front/ChangeCoin/src/Logica/OptionsConvertion.java
@@ -1,8 +1,8 @@
 package Logica;
 
 public enum OptionsConvertion {
-    EXCHANGE_MONY("Exchange Mony"),
-    CHANGE_DIGITAL_CURRENCY("Change Digital Currency");
+    EXCHANGE_MONY("Exchange Mony");
+   //CHANGE_DIGITAL_CURRENCY("Change Digital Currency");
     private final String getCurrencyName;
     OptionsConvertion(String currencyName) {
         this.getCurrencyName = currencyName;
