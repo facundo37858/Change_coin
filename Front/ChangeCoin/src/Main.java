a8a1a59cddf0ff9d56cbe5138cabca4cd2dfc421
facundo2309 <facundoomargarcia@gmail.com>
2022-12-07 12:38:47 -0300

    Program flow

--- Front/ChangeCoin/src/Main.java
+++ Front/ChangeCoin/src/Main.java
@@ -1,20 +1,50 @@
 import Logica.*;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public void inicio(){
+        //Comienzo programa
+        //Selecion del tipo de convercion
         OptionsConvertion[] optionConvertions = OptionsConvertion.values();
         ShowMultipleSelector optionsConvertions = new ShowMultipleSelector(optionConvertions);
         String optionConvertSelected = optionsConvertions.showMultipleSelctorValues("Choose a conversion option");
 
-        ShowInputDialog input = new ShowInputDialog("Enter value");
-
-
+        if(optionConvertSelected == null){
+            ShowDialogMessage finish = new ShowDialogMessage();
+            finish.showDialogFinish();
+            System.exit(200);
+        }
+        //Seleccionar tipo de cambio
         Currencies[] optionCurrency = Currencies.values();
         ShowMultipleSelector optionsCurrency = new ShowMultipleSelector(optionCurrency);
         String optionCurrencySelected = optionsCurrency.showMultipleSelctorValues("Convert to:");
 
-        double value = Integer.parseInt(input.getValue());
+        if( optionCurrencySelected == null){
+            ShowDialogMessage dialoog = new ShowDialogMessage();
+            int response = dialoog.showDialog("¿Desea continuar?");
+            if(response == 0){
+                inicio();
+            }else {
+                ShowDialogMessage finishDialog = new ShowDialogMessage();
+                finishDialog.showDialogFinish();
+                System.exit(200);
+            }
+        }
+        //Enter dato
+        ShowInputDialog input = new ShowInputDialog("Enter value");
+        String responseInputDialog = input.getValue();
+        if(responseInputDialog == null){
+            ShowDialogMessage dialoog = new ShowDialogMessage();
+            int respose = dialoog.showDialog("¿Desea continuar?");
+            if(respose == 0){
+                inicio();
+            }else {
+                ShowDialogMessage finishDialog = new ShowDialogMessage();
+                finishDialog.showDialogFinish();
+                System.exit(200);
+            }
+        }
+        double value = Integer.parseInt(responseInputDialog);
         double converted = optionsCurrency.convetion(optionCurrencySelected);
 
         ShowInfo info = new ShowInfo(value,converted);
@@ -22,5 +52,20 @@ public class Main {
         double result = info.getResult();
         info.showValue(result,optionCurrencySelected);
 
+        ShowDialogMessage dialoog = new ShowDialogMessage();
+        int respose = dialoog.showDialog("¿Desea continuar?");
+        System.out.println(respose);
+        if(respose == 0){
+            inicio();
+        }else {
+            ShowDialogMessage finishDialog = new ShowDialogMessage();
+            finishDialog.showDialogFinish();
+            System.exit(200);
+        }
+    }
+
+    public static void main(String[] args) {
+       Main inicio = new Main();
+       inicio.inicio();
     }
 }
